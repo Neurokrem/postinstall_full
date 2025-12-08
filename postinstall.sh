7b974507674dc1ef84b9b7b811cbf22fcb1d71e8
Neurokrem <neurokrem@protonmail.com>
2025-12-08 20:21:51 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "Running apt installer..."
 bash "$REPO_DIR/apt/install.sh"
